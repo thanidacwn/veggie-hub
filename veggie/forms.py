8a9a1505b6eb2cd4958ab128693b5e72761515ca
thanidacwn <92778351+thanidacwn@users.noreply.github.com>
2023-10-17 22:20:31 +0700

    create models

---
+++ veggie/forms.py
@@ -0,0 +1,10 @@
+from .models import *
+from django.forms import ModelForm
+
+
+class ReviewForm(ModelForm):
+    class Meta:
+        model = Review
+        fields = [restaurant, review_user, review_title,
+                review_description, review_rate, price_rate, review_date]
+
