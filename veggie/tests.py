abf6a0f557a0b1afc1d9483ca6db84749598a780
thanidacwn <92778351+thanidacwn@users.noreply.github.com>
2023-10-18 00:26:02 +0700

    Initial test

--- veggie/tests.py
+++ veggie/tests.py
@@ -1,3 +1,11 @@
 from django.test import TestCase
+from django.urls import reverse
+from .models import Category, State, Restaurant, Review
+from django.contrib.auth.models import User
 
-# Create your tests here.
+
+class ModelTestCase(TestCase):
+    pass
+
+if __name__ == '__main__':
+    unittest.main()
