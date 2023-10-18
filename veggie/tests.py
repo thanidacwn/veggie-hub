bad0c74367488828257d03a441488b9678df136d
thanidacwn <92778351+thanidacwn@users.noreply.github.com>
2023-10-18 16:24:46 +0700

    test for models

--- veggie/tests.py
+++ veggie/tests.py
@@ -1,11 +1,50 @@
 from django.test import TestCase
-from django.urls import reverse
 from .models import Category, State, Restaurant, Review
 from django.contrib.auth.models import User
 
+class RestaurantModelTest(TestCase):
+    """Test Restaurant model"""
+    def setUp(self):
+        """Set up test data"""
+        self.category = Category.objects.create(category_text="Test Category")
+        self.state = State.objects.create(state_text="Test State")
+        self.user = User.objects.create_user(username="testuser", password="testpassword")
+        self.restaurant = Restaurant.objects.create(
+            restaurant_text="Test Restaurant",
+            category=self.category,
+            state=self.state,
+            city="Test City",
+            location="Test Location",
+            restaurant_link="https://example.com/restaurant",
+            menu_link="https://example.com/menu",
+            price_rate="expensive"
+        )
+        self.review = Review.objects.create(
+            restaurant=self.restaurant,
+            review_user=self.user,
+            review_title="Test Review",
+            review_description="This is a test review.",
+            review_rate=4
+        )
+        self.review2 = Review.objects.create(
+            restaurant=self.restaurant,
+            review_user=self.user,
+            review_title="Test Review 2",
+            review_description="This is a test review 2.",
+            review_rate=3
+        )
+        self.review3 = Review.objects.create(
+            restaurant=self.restaurant,
+            review_user=self.user,
+            review_title="Test Review 3",
+            review_description="This is a test review 3.",
+            review_rate=1
+        )
 
-class ModelTestCase(TestCase):
-    pass
+    def test_average_rate(self):
+        """Test average rate"""
+        self.assertEqual(self.restaurant.get_average_rate, 2.67)
 
-if __name__ == '__main__':
-    unittest.main()
+    def test_reviews_amount(self):
+        """Test reviews amount"""
+        self.assertEqual(self.restaurant.get_reviews_amount, 3)
