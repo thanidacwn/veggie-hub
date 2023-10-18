83aa706c2638db40b4de247da2c7219e60e74ed3
thanidacwn <92778351+thanidacwn@users.noreply.github.com>
2023-10-18 12:03:48 +0700

    Get data from csv file and save to database

--- veggie/data.py
+++
@@ -1,6 +0,0 @@
-import requests
-
-url = "https://github.com/thanidacwn/veggie-data/blob/master/last_data.csv"
-response = requests.get(url)
-print(response.json())
-
--- veggie/views.py
+++ veggie/views.py
@@ -1,5 +1,29 @@
 from django.http import HttpResponse
+from .models import Category, State, Restaurant
+import pandas as pd
+import ssl
 
 
 def index(request):
+    """Return the index page."""
     return HttpResponse("Hello, world. You're at the index page.")
+
+
+def home(request):
+    """Get data from csv file and save to database."""
+    ssl._create_default_https_context = ssl._create_unverified_context
+    df = pd.read_csv('https://raw.githubusercontent.com/thanidacwn/veggie-data/master/last_data.csv')
+    for index, row in df.iterrows():
+        all_category = row["category"].split(", ")
+        for cate in all_category:
+            category = Category.objects.get_or_create(category_text=row["category"])[0]
+        state = State.objects.get_or_create(state_text=row["state"])[0]
+        restaurant = Restaurant(restaurant_text=row["restaurant_text"], 
+                        category=category,
+                        state=state, city=row["city"],
+                        location=row["location"],
+                        restaurant_link=row["restaurant_link"],
+                        menu_link=row["menu_link"],
+                        price_rate=row["price_rate"])
+        restaurant.save()
+    return HttpResponse("Hello, world. You're at the home page.")
