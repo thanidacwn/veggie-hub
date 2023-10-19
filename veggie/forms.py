875cbe615d20fbced595ebcfb5b6e3d959a6ad83
thanidacwn <92778351+thanidacwn@users.noreply.github.com>
2023-10-20 00:00:57 +0700

    add string to fields

--- veggie/forms.py
+++ veggie/forms.py
@@ -5,6 +5,6 @@ from django.forms import ModelForm
 class ReviewForm(ModelForm):
     class Meta:
         model = Review
-        fields = [restaurant, review_user, review_title,
-                review_description, review_rate, review_date]
+        fields = ["restaurant", "review_user", "review_title",
+                "review_description", "review_rate", "review_date"]
 
