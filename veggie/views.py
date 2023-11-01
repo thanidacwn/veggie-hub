8620adeb3eec391534188325eeb9bbc0c1a3f07d
thanidacwn <92778351+thanidacwn@users.noreply.github.com>
2023-11-01 11:04:43 +0700

    rename method

--- veggie/views.py
+++ veggie/views.py
@@ -124,26 +124,24 @@ class GetRestaurantByCategoryAndState(generic.ListView):
         )
 
 
-def home(request):
+def get_data(request):
     """Get data from csv file and save to database."""
     ssl._create_default_https_context = ssl._create_unverified_context
     df = pd.read_csv('https://raw.githubusercontent.com/thanidacwn/veggie-data/master/last_data.csv')
     for index, row in df.iterrows():
         all_category = row["category"].split(", ")
-        for _ in all_category:
+        for cate in all_category:
             category = Category.objects.get_or_create(category_text=row["category"])[0]
-            state = State.objects.get_or_create(state_text=row["state"])[0]
-            restaurant = Restaurant(restaurant_text=row["restaurant_text"],
+        state = State.objects.get_or_create(state_text=row["state"])[0]
+        restaurant = Restaurant(restaurant_text=row["restaurant_text"], 
                         category=category,
                         state=state, city=row["city"],
                         location=row["location"],
                         restaurant_link=row["restaurant_link"],
                         menu_link=row["menu_link"],
-                        price_rate=row["price_rate"],
-                        image=row["image"])
-            
-            restaurant.save()
-    return HttpResponse("Hello, world. You're at the home page.")
+                        price_rate=row["price_rate"])
+        restaurant.save()
+    return HttpResponse("Hello, world. You got the data!")
 
 
 class DetailView(generic.DetailView):
