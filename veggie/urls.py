b050141032fed7b45a724d2cae6392e4a1fb254a
PanupunJanin <panupan.ja@gmail.com>
2023-11-01 20:19:16 +0700

    Add my reviews url and nav bar link

--- veggie/urls.py
+++ veggie/urls.py
@@ -8,5 +8,6 @@ urlpatterns = [
     path('restaurant/<int:pk>/', views.DetailView.as_view(), name='detail'),
     path("", views.RestaurantsView.as_view(), name="index"),
     path("restaurant/<int:pk>/add_review", views.add_review, name="add_review"),
+    path('<int:pk>/myvotes/', views.MyReviews.as_view(), name='my_reviews'),
     path('filter/', views.GetRestaurantByCategoryAndState.as_view(), name='filtered_category_state'),
 ]
