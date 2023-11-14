7b8ee25cecc7f8fa8da43f98082a52599b095a5d
thanidacwn <92778351+thanidacwn@users.noreply.github.com>
2023-11-14 17:18:16 +0700

    setting staticsfiles

--- mysite/settings.py
+++ mysite/settings.py
@@ -12,9 +12,10 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 
 from pathlib import Path
 from decouple import config, Csv
+import os
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
-BASE_DIR = Path(__file__).resolve().parent.parent
+BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 
 
 # Quick-start development settings - unsuitable for production
@@ -132,7 +133,7 @@ ACCOUNT_LOGIN_ATTEMPTS_TIMEOUT = 500
 DATABASES = {
     "default": {
         "ENGINE": "django.db.backends.sqlite3",
-        "NAME": BASE_DIR / "db.sqlite3",
+        "NAME": os.path.join(BASE_DIR, "db.sqlite3"),
     }
 }
 
@@ -179,7 +180,8 @@ NPM_BIN_PATH = config('NPM_BIN_PATH', cast=str, default='/usr/local/bin/npm')
 # Static files (CSS, JavaScript, Images)
 # https://docs.djangoproject.com/en/4.1/howto/static-files/
 
-STATIC_URL = "static/"
+STATIC_URL = "/static/"
+STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 # Default primary key field type
 # https://docs.djangoproject.com/en/4.1/ref/settings/#default-auto-field
