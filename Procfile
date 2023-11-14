0fd885c9bc92d0d321d7ac34fc6c0bbdac6103b4
thanidacwn <92778351+thanidacwn@users.noreply.github.com>
2023-11-14 23:20:23 +0700

    add procfile to deploy

---
+++ Procfile
@@ -0,0 +1,1 @@
+web: gunicorn veggie-hub.wsgi
--- mysite/settings.py
+++ mysite/settings.py
@@ -13,6 +13,12 @@ https://docs.djangoproject.com/en/4.1/ref/settings/
 from pathlib import Path
 from decouple import config, Csv
 import os
+import django_heroku
+
+
+# Activate Django-Heroku.
+django_heroku.settings(locals())
+TEST_RUNNER = 'django_heroku.HerokuDiscoverRunner'
 
 # Build paths inside the project like this: BASE_DIR / 'subdir'.
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
