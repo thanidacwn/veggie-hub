893a93d90811ab386282bd865c0c635841dc618b
thanidacwn <92778351+thanidacwn@users.noreply.github.com>
2023-11-15 22:50:47 +0700

    set web name

--- mysite/settings.py
+++ mysite/settings.py
@@ -202,4 +202,6 @@ STATIC_ROOT = os.path.join(BASE_DIR, 'staticfiles')
 
 DEFAULT_AUTO_FIELD = "django.db.models.BigAutoField"
 
+CSRF_TRUSTED_ORIGINS = ["https://veggie-hub.herokuapp.com", "http://127.0.0.1"]
+
 django_heroku.settings(locals())
